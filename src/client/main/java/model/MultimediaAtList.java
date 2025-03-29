f957afa7e521c28e584b85b960ee021eea09560a
JoseGS <josegs.95@hotmail.com>
2025-03-29 20:06:01 +0100

    Get user lists from database

---
+++ src/client/main/java/model/MultimediaAtList.java
@@ -0,0 +1,37 @@
+package model;
+
+public class MultimediaAtList {
+    final private Multimedia multimedia;
+    private MultimediaStatus status;
+    private int current_episode;
+
+    public MultimediaAtList(Multimedia multimedia, MultimediaStatus status, int current_episode) {
+        this.multimedia = multimedia;
+        this.status = status;
+        this.current_episode = current_episode;
+    }
+
+    public MultimediaAtList(Multimedia multimedia, MultimediaStatus status) {
+        this(multimedia, status, 0);
+    }
+
+    public Multimedia getMultimedia() {
+        return multimedia;
+    }
+
+    public MultimediaStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(MultimediaStatus status) {
+        this.status = status;
+    }
+
+    public int getCurrent_episode() {
+        return current_episode;
+    }
+
+    public void setCurrent_episode(int current_episode) {
+        this.current_episode = current_episode;
+    }
+}
